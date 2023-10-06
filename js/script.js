6881f9bfd8f7536cd5d11175ef40f9cd586ab27d
Luciano Silva <luciano_jet@hotmail.com>
2023-10-05 23:48:38 -0300

    taquero

--- js/script.js
+++ js/script.js
@@ -9,9 +9,10 @@ document.addEventListener("DOMContentLoaded", function () {
       const nome = document.querySelector(".nome").value;
       const telefone = document.querySelector(".telefone").value;
       const mensagem = document.querySelector(".mensagem").value;
+      const escolha = document.querySelector(".escolha")
   
       // Formatar a mensagem para envio no WhatsApp
-      const mensagemFormatada = `Seja Bem Vindo(a)!%0A%0A**Informações do Contato**%0A%0A*Nome:* ${nome}%0A%0A*Telefone:* ${telefone}%0A%0A*Mensagem:*%0A${mensagem}`;
+      const mensagemFormatada = `Seja Bem Vindo(a)!%0A%0A**Informações do Contato**%0A%0A*Nome:* ${nome}%0A%0A*Telefone:* ${telefone}%0A%0A*Tipo de Serviço: ${escolha}*0A%0A*Mensagem:*%0A%0A${mensagem}`;
   
       // Número de telefone para o qual você deseja enviar a mensagem via WhatsApp
       const numeroWhatsApp = "5598987189453";
@@ -26,3 +27,18 @@ document.addEventListener("DOMContentLoaded", function () {
   });
   
 
+
+
+  function enviarWhatsApp() {
+    const servicoSelecionado = document.getElementById("escolha").value;
+    const numeroDestino = 'SEU_NUMERO_DE_TELEFONE'; // Substitua pelo seu número de telefone com o código do país (exemplo: +5511999999999)
+    
+    // Monta a mensagem com a escolha do serviço
+    const mensagem = `Olá, gostaria de agendar um serviço de ${servicoSelecionado}.`;
+
+    // Cria o link do WhatsApp com a mensagem
+    const linkWhatsApp = `https://api.whatsapp.com/send?phone=${numeroDestino}&text=${encodeURIComponent(mensagem)}`;
+
+    // Abre o link no WhatsApp
+    window.open(linkWhatsApp);
+}
