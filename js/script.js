0940d6588043a9e29e1f574f078a5974db83748b
Luciano Silva <luciano_jet@hotmail.com>
2023-10-06 00:49:37 -0300

    teste

--- js/script.js
+++ js/script.js
@@ -19,26 +19,13 @@ document.addEventListener("DOMContentLoaded", function () {
   
       // URL do WhatsApp com a mensagem formatada
       const urlWhatsApp = `https://wa.me/${numeroWhatsApp}?text=${mensagemFormatada}`;
-  
+
+    
       // Abre uma nova aba ou janela do navegador para enviar a mensagem via WhatsApp
       window.open(urlWhatsApp);
-      console.log(mensagemFormatada)
+
+      alert("Mensagem enviada com sucesso!");
     });
   });
   
 
-
-
-//   function enviarWhatsApp() {
-//     const servicoSelecionado = document.getElementById("escolha").value;
-//     const numeroDestino = 'SEU_NUMERO_DE_TELEFONE'; // Substitua pelo seu número de telefone com o código do país (exemplo: +5511999999999)
-    
-//     // Monta a mensagem com a escolha do serviço
-//     const mensagem = `Olá, gostaria de agendar um serviço de ${servicoSelecionado}.`;
-
-//     // Cria o link do WhatsApp com a mensagem
-//     const linkWhatsApp = `https://api.whatsapp.com/send?phone=${numeroDestino}&text=${encodeURIComponent(mensagem)}`;
-
-//     // Abre o link no WhatsApp
-//     window.open(linkWhatsApp);
-// }
