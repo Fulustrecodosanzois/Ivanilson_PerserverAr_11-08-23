8cbe14b896083c35c04d4b626d267c82475dfada
Luciano Silva <luciano_jet@hotmail.com>
2023-10-06 12:43:45 -0300

    ajustado

--- js/script.js
+++ js/script.js
@@ -1,36 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Seletor do botão "Enviar"
     const enviarBtn = document.querySelector(".enviar-btn");
   
     enviarBtn.addEventListener("click", function (e) {
       e.preventDefault();
-  
-      // Coletar dados do formulário
+
       const nome = document.querySelector(".nome").value;
       const telefone = document.querySelector(".telefone").value;
       const mensagem = document.querySelector(".mensagem").value;
       const escolha = document.querySelector(".escolha").value
   
-      // Formatar a mensagem para envio no WhatsApp
       const mensagemFormatada = `Seja Bem Vindo(a)!%0A%0A**Informações do Contato**%0A%0A*Nome:* ${nome}%0A%0A*Telefone:* ${telefone}%0A%0A*Tipo de Serviço:* ${escolha}%0A%0A*Mensagem:*%0A%0A${mensagem}`;
   
-      // Número de telefone para o qual você deseja enviar a mensagem via WhatsApp
       const numeroWhatsApp = "5598987189453";
   
-      // URL do WhatsApp com a mensagem formatada
       const urlWhatsApp = `https://wa.me/${numeroWhatsApp}?text=${mensagemFormatada}`;
 
-      const alertaEnvio = document.createElement("div");
-      alertaEnvio.style.fontSize = "18px"; // Tamanho da fonte
-      alertaEnvio.textContent = "Mensagem enviada com sucesso!";
-      alert(alertaEnvio.textContent);
-
-      // alert("Sua mensagem já esta PRONTA. somente confirme o envio da mensagem no WhatsApp.");
+      alert("Sua mensagem já esta PRONTA. somente envie a mensagem no WhatsApp.");
   
       window.open(urlWhatsApp);
-
       
     });
   });
   
-
